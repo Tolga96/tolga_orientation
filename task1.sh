54143a30cedb6bdd99d293a2d451f72ef38d3cb8
tolga <tcandemir97@gmail.com>
2020-07-01 01:50:57 +0300

    this is my first commit

---
+++ task1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for x in 1 2 3 4 5
+do
+touch ~/file1/tolga${x}.txt && echo "hello\nworld\ndeneme1" >> ~/file1/tolga${x}.txt
+touch ~/file2/tolga${x}.txt && echo "hello\nworld\ndeneme2" >> ~/file2/tolga${x}.txt
+mv ~/file2/tolga${x}.txt ~/file2/tolga${x}.txt.old
+cp ~/file1/tolga${x}.txt ~/file2/tolga${x}.txt
+done
+
+echo "Process is succesfully"
+
+
